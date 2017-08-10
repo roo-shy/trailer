47431a30996643ab443047500e9052cf7c9ac00c
at-rooshee <ru_sha_nia@yahoo.ca>
2017-08-09 22:54:40 -0500

    Editing p and h2

--- src/App.js
+++ src/App.js
@@ -17,11 +17,10 @@ import './App.css';
        <div className="App">
          <div className="App-header">
            <img src={logo} className="App-logo" alt="logo" />
-           <h2>Welcome to React</h2>
+           
          </div>
          <p className="App-intro">
-           To get started, edit <code>src/App.js</code> and save to reload.
-         </p>
+        </p>
          <RaisedButton label="Material UI" onClick={() => this.handleClick()} />
        </div>
        </MuiThemeProvider>
