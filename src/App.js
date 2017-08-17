7b559460997ab790594f3bc30a00f6ddab17874f
at-rooshee <ru_sha_nia@yahoo.ca>
2017-08-16 23:43:19 -0500

    Adding redux list

--- src/App.js
+++ src/App.js
@@ -3,27 +3,43 @@ import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
+import AddTodo from './containers/AddTodo'
+import TodoList from './containers/TodoList'
 
-
- class App extends React.Component {
-   
-   handleClick = () => {
-     console.log("pri");
-   }
-   
-  render() {
-    return (
-      <MuiThemeProvider>
-       <div className="App">
-         <div className="App-header">
-           <img src={logo} className="App-logo" alt="logo" />
-         </div>
-         <p className="App-intro"></p>
-         <RaisedButton label="Material UI" onClick={() => this.handleClick()} />
-       </div>
-       </MuiThemeProvider>
-    );
-  }
-}
+const App = () => (
+  
+    <MuiThemeProvider>
+      <div>
+        <h1>Hey</h1>
+    {/* <AddTodo />
+    <TodoList /> */}
+    </div>
+  </MuiThemeProvider>
+  
+)
+//  class App extends React.Component {
+//    
+//    handleClick = () => {
+//      console.log("pri");
+//    }
+//    
+//   render() {
+//     return (
+//       <MuiThemeProvider>
+//        <div className="App">
+//          <div className="App-header">
+//            <img src={logo} className="App-logo" alt="logo" />
+//          </div>
+//          <Paper zDepth={2}>
+//           <TextField hintText="Add todo" underlineShow={false} />
+//           <Divider />
+//         </Paper>
+//          <p className="App-intro"></p>
+//          <RaisedButton label="Material UI" onClick={() => this.handleClick()} />
+//        </div>
+//        </MuiThemeProvider>
+//     );
+//   }
+// }
 
 export default App;
